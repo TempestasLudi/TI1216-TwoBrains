297fc7dc3c1c4b18001c5a14eb000c5c297463c2
Stefan van der Heijden <stefan0806@live.nl>
2015-11-19 15:09:45 +0100

    Hierbij de klassen Grade en Match

--- src/Match.java
+++ src/Match.java
@@ -1,4 +1,32 @@
 
-public class Match {
-
+public class Match 
+{
+	private User user1;
+	private User user2;
+	
+	public Match(User user1, User user2)
+	{
+		this.user1 = user1;
+		this.user2 = user2;
+	}
+	
+	public User getUser1()
+	{
+		return this.user1;
+	}
+	
+	public User getUser2()
+	{
+		return this.user2;
+	}
+	
+	public boolean equals(Object other)
+	{
+		if(other instanceof Match)
+		{
+			Match that = (Match) other;
+			return ((this.user1.equals(that.user1)) && this.user2.equals(that.user2));
+		}
+		return false;
+	}
 }
