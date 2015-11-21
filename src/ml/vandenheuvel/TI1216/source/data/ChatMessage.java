ba90ccd1edf14ccd0010c68d3703b1ee9c461cfd
Al5tfrq3BRrIbV <heuveltje@hotmail.com>
2015-11-21 16:56:50 +0100

    Extended the ChatMessage class to include a sender and receiver. Made the signUp bool in the Login class private and created get and set methods.

--- src/ml/vandenheuvel/TI1216/source/data/ChatMessage.java
+++ src/ml/vandenheuvel/TI1216/source/data/ChatMessage.java
@@ -2,30 +2,39 @@ package ml.vandenheuvel.TI1216.source.data;
 
 /**
  * Class "ChatMessage".
- * @author Andreas Theys, OOP Project [TI1216], Project Group A1.2, TU Delft 2015-2016. 
+ * 
+ * @author Andreas Theys, OOP Project [TI1216], Project Group A1.2, TU Delft
+ *         2015-2016.
  */
 
 public class ChatMessage {
 
-	
 	/**
 	 * Class-instances/variables.
 	 */
+	String sender;
 	String message;
-	
+	String receiver;
+
 	// BEGIN CONSTRUCTORS
-	
+
 	/**
 	 * Default Constructor
 	 */
-	public ChatMessage(){}
-	
+	public ChatMessage() {
+	}
+
 	/**
 	 * General constructor.
-	 * @param message
+	 * 
+	 * @param sender	The username of the person who sends the message
+	 * @param message	The actual content of the message
+	 * @param receiver	The username of the person who should receive the message
 	 */
-	public ChatMessage(String message){
+	public ChatMessage(String sender, String message, String receiver) {
+		this.sender = sender;
 		this.message = message;
-		}
-			
+		this.receiver = receiver;
+	}
+
 }
--- src/ml/vandenheuvel/TI1216/source/data/Login.java
+++ src/ml/vandenheuvel/TI1216/source/data/Login.java
@@ -2,34 +2,55 @@ package ml.vandenheuvel.TI1216.source.data;
 
 /**
  * Class "login".
- * @author Andreas Theys, OOP Project [TI1216], Project Group A1.2, TU Delft 2015-2016. 
+ * 
+ * @author Andreas Theys, OOP Project [TI1216], Project Group A1.2, TU Delft
+ *         2015-2016.
  */
 public class Login {
-	
+
 	/**
 	 * Class-instances/variables.
+	 * signUp is true when signing up, false when logging in.
 	 */
 	private String username;
 	private String password;
-	public boolean signUp;
-	
-	
+	private boolean signUp;
+
 	// BEGIN CONSTRUCTORS
-	
+
 	/**
 	 * Default Constructor
 	 */
-	public Login(){}
-	
+	public Login() {
+	}
+
 	/**
 	 * General constructor. All instances given.
-	 * @param username
-	 * @param password
-	 * @param alreadyExists
+	 * 
+	 * @param username	The username
+	 * @param password	The password
+	 * @param singUp	Is true when signing up a new account, false if logging in.
 	 */
-	public Login(String username, String password, boolean signUp){
-		this.username = username; this.password = password; 
+	public Login(String username, String password, boolean signUp) {
+		this.username = username;
+		this.password = password;
 		this.signUp = signUp;
-		}
+	}
 	
+	/**
+	 * Get method of the signUp boolean
+	 * @return	True if the person wants to sign up, false if just logging in.
+	 */
+	public boolean getSignUp(){
+		return this.signUp;
+	}
+	
+	/**
+	 * Set method of the signUp boolean
+	 * @param bool_in The wanted value
+	 */
+	public void setSignUp(Boolean bool_in){
+		this.signUp = bool_in;
+	}
+
 }
