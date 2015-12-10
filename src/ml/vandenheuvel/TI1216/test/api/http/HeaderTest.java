85efbce50d478daec8a2d376f816fc6179ce46a2
atheys <andreastheys@gmail.com>
2015-12-10 12:03:52 +0100

    Default and first Header-constructor tested. Second constructor testing initiated.

--- src/ml/vandenheuvel/TI1216/test/api/http/HeaderTest.java
+++ src/ml/vandenheuvel/TI1216/test/api/http/HeaderTest.java
@@ -10,35 +10,90 @@ import ml.vandenheuvel.TI1216.source.api.http.*;
 
 public class HeaderTest {
 
+	
 	@Test
 	public void DefaultConstructorTest() {
-		ResponseLine r1 = new ResponseLine("HTTP/1.1", "200", "OK");
-		String r1String = r1.toString();
-		HeaderField r1HeaderField = new HeaderField("Connection","close"); 
-		String r1HeaderFieldString = r1HeaderField.toString();
+		ResponseLine r = new ResponseLine("HTTP/1.1", "200", "OK");
+		String rString = r.toString();
+		HeaderField rHeaderField = new HeaderField("Connection","close"); 
+		String rHeaderFieldString = rHeaderField.toString();
 		
-		Header h1 = new Header();
-		String h1HeaderLine = h1.getHeaderLine().toString();
-		String h1HeaderField = h1.getField("Connection").toString();
+		Header h = new Header();
+		String hHeaderLine = h.getHeaderLine().toString();
+		String hHeaderField = h.getField("Connection").toString();
 		
-		boolean evaluate = r1String.equals(h1HeaderLine) && r1HeaderFieldString.equals(h1HeaderField) ;
+		boolean evaluate = rString.equals(hHeaderLine) && rHeaderFieldString.equals(hHeaderField) ;
+		assertTrue(evaluate);
+	}
+	
+	
+	@Test
+	public void testFirstConstructor1(){
+		HeaderField hf = new HeaderField("Connection","close"); 
+		String hfString = hf.toString();
+		
+		HeaderLine hl = new ResponseLine("HTTP/1.1", "200", "OK");
+		String hlString = hl.toString();
+		
+		Header h = new Header(hl);
+		String hHeaderLine = h.getHeaderLine().toString();
+		String hHeaderField = h.getField("Connection").toString();
+		
+		boolean evaluate = hlString.equals(hHeaderLine) && hfString.equals(hHeaderField);
+		assertTrue(evaluate);
+	}
+	
+	
+	@Test
+	public void testFirstConstructor2(){
+		HeaderLine hl = new ResponseLine("HTTP/1.1", "200", "OK");
+		HeaderField hf = new HeaderField("Connection","close"); 
+		Header h = new Header(hl);
+
+		boolean evaluate = hl.equals(h.getHeaderLine()) && hf.equals(h.getField("Connection"));
+		assertTrue(evaluate);
+	}
+	
+	
+	@Test
+	public void testFirstConstructor3(){
+		
+		HeaderLine hl = new RequestLine("method", "URI", "version");
+		String hlString = hl.toString();
+		
+		Header h = new Header(hl);
+		String hHeaderLine = h.getHeaderLine().toString();
+		
+		boolean evaluate = hlString.equals(hHeaderLine) && h.getFields().equals(new HeaderField[0]);
+		assertTrue(evaluate);
+	}
+	
+	
+	@Test
+	public void testFirstConstructor4(){
+		HeaderLine hl = new RequestLine("method", "URI", "version");
+		Header h = new Header(hl);
+		
+		boolean evaluate = hl.equals(h.getHeaderLine())&& h.getFields().equals(new HeaderField[0]);
 		assertTrue(evaluate);
 	}
 	
 	// To do --> Andreas
 	@Test
-	public void ConstructorTest1(){}
+	public void testSecondConstructor1(){}
+
 	
 	// To do --> Andreas
 	@Test
-	public void ConstructorTest2(){}
+	public void testSecondConstructor2(){}
+	
 	
 	// To do --> Andreas
 	@Test
-	public void ConstructorTest3(){}
+	public void testSecondConstructor3(){}
+	
 	
 	// To do --> Andreas
 	@Test
-	public void ConstructorTest4(){}
-	
+	public void testSecondConstructor4(){}
 }
