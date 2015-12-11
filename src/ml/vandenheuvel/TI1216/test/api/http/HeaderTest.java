d3f8527ef802bd44e3972452e2b32f2d7e2841f6
atheys <andreastheys@gmail.com>
2015-12-12 00:21:10 +0100

    HeaderTest - constructor tests finalized; getter test methods initiated

--- src/ml/vandenheuvel/TI1216/test/api/http/HeaderTest.java
+++ src/ml/vandenheuvel/TI1216/test/api/http/HeaderTest.java
@@ -78,43 +78,317 @@ public class HeaderTest {
 	}
 	
 	
-	// To do --> Andreas
 	@Test
 	public void testSecondConstructor1(){
 		RequestLine rl = new RequestLine("method", "URI", "version");
-		HeaderField hf = new HeaderField("Connection","close"); 
+		
+		HeaderField hf1 = new HeaderField("Connection","close"); 
+		HeaderField hf2 = new HeaderField("Server","working"); 
 		
 		List<HeaderField> fields = new ArrayList<HeaderField>();
-		fields.add(hf);
-		
-		HeaderField[] afields = new HeaderField[fields.size()];
-		fields.toArray(afields);
+		fields.add(hf1);
+		fields.add(hf2);
 		
 		Header h = new Header(rl, fields);
 		boolean evaluate1 = h.getHeaderLine().equals(rl);
 		
 		boolean evaluate2 = true;
 		for(int i=0; i<fields.size();i++){
-			evaluate2 = evaluate2 && fields.get(i).equals();
-		}
+			evaluate2 = evaluate2 && fields.get(i).toString().equals(h.getFields()[i].toString());
+			}
 		
-		
-		assertTrue();
+		assertTrue(evaluate1&&evaluate2);
 		
 	}
 
 	
+	@Test
+	public void testSecondConstructor2(){
+		RequestLine rl = new RequestLine("method", "URI", "version");
+		
+		HeaderField hf1 = new HeaderField("Connection","close"); 
+		HeaderField hf2 = new HeaderField("Server","working"); 
+		
+		List<HeaderField> fields = new ArrayList<HeaderField>();
+		fields.add(hf1);
+		fields.add(hf2);
+		
+		Header h = new Header(rl, fields);
+		boolean evaluate1 = h.getHeaderLine().equals(rl);
+		
+		boolean evaluate2 = true;
+		for(int i=0; i<fields.size();i++){
+			evaluate2 = evaluate2 && fields.get(i).equals(h.getFields()[i]);
+			}
+		
+		assertTrue(evaluate1&&evaluate2);	
+	}
+	
+	
+	@Test
+	public void testGetFields1(){
+		HeaderLine hl = new ResponseLine("HTTP/1.1", "200", "OK");
+		HeaderField hf = new HeaderField("Connection","close"); 
+		Header h = new Header(hl);
+		
+		HeaderField[] hfs1 = {hf};
+		HeaderField[] hfs2 = h.getFields();
+		
+		boolean evaluate = true && (hfs1.length == hfs2.length);
+		for(int i=0; i<hfs2.length;i++){
+			evaluate = evaluate && hfs2[i].toString().equals(hfs1[i].toString());
+			}
+		
+		assertTrue(evaluate);
+	}
+	
+	@Test
+	public void testGetFields2(){
+		HeaderLine hl = new ResponseLine("HTTP/1.1", "200", "OK");
+		HeaderField hf = new HeaderField("Connection","close"); 
+		Header h = new Header(hl);
+		
+		HeaderField[] hfs1 = {hf};
+		HeaderField[] hfs2 = h.getFields();
+		
+		boolean evaluate = true && (hfs1.length == hfs2.length);
+		for(int i=0; i<hfs2.length;i++){
+			evaluate = evaluate && hfs2[i].equals(hfs1[i]);
+			}
+		
+		assertTrue(evaluate);
+	}
+	
+	
+	@Test
+	public void testGetFields3(){
+		HeaderLine hl = new RequestLine("method", "URI", "version");
+		Header h = new Header(hl);
+		
+		HeaderField[] hfs1 = new HeaderField[h.getFields().length];
+		HeaderField[] hfs2 = h.getFields();
+		
+		boolean evaluate = true && (hfs1.length == hfs2.length);
+		for(int i=0; i<hfs2.length;i++){
+			evaluate = evaluate && hfs2[i].equals(hfs1[i]);
+			}
+		
+		assertTrue(evaluate);	
+	}
+	
+	
+	@Test
+	public void testGetFields4(){
+		HeaderLine hl = new RequestLine("method", "URI", "version");
+		Header h = new Header(hl);
+		
+		HeaderField[] hfs1 = new HeaderField[h.getFields().length];
+		HeaderField[] hfs2 = h.getFields();
+		
+		boolean evaluate = true && (hfs1.length == hfs2.length);
+		for(int i=0; i<hfs2.length;i++){
+			evaluate = evaluate && hfs2[i].toString().equals(hfs1[i].toString());
+			}
+		
+		assertTrue(evaluate);	
+	}
+	
+	
+	@Test
+	public void testGetFields5(){
+		RequestLine rl = new RequestLine("method", "URI", "version");
+		HeaderField hf1 = new HeaderField("Connection","close"); 
+		HeaderField hf2 = new HeaderField("Server","working"); 
+		HeaderField hf3 = new HeaderField("Server","lazy"); 
+		
+		List<HeaderField> fields = new ArrayList<HeaderField>();
+		fields.add(hf1);
+		fields.add(hf2);
+		fields.add(hf3);
+		
+		Header h = new Header(rl, fields);
+		
+		HeaderField[] hfs1 = {hf1,hf2,hf3};
+		HeaderField[] hfs2 = h.getFields();
+		
+		boolean evaluate = true && (hfs1.length == hfs2.length);
+		for(int i=0; i<hfs2.length;i++){
+			evaluate = evaluate && hfs2[i].toString().equals(hfs1[i].toString());
+			}
+		
+		assertTrue(evaluate);
+		
+	}
+	
+	
+	@Test
+	public void testGetFields6(){
+		RequestLine rl = new RequestLine("method", "URI", "version");
+		HeaderField hf1 = new HeaderField("Connection","close"); 
+		HeaderField hf2 = new HeaderField("Server","working"); 
+		HeaderField hf3 = new HeaderField("Server","lazy"); 
+		HeaderField hf4 = new HeaderField("Server","wrong");
+		
+		List<HeaderField> fields = new ArrayList<HeaderField>();
+		fields.add(hf1);
+		fields.add(hf2);
+		fields.add(hf4);
+		
+		Header h = new Header(rl, fields);
+		
+		HeaderField[] hfs1 = {hf1,hf2,hf3};
+		HeaderField[] hfs2 = h.getFields();
+		
+		boolean evaluate = true && (hfs1.length == hfs2.length);
+		for(int i=0; i<hfs2.length;i++){
+			evaluate = evaluate && hfs2[i].toString().equals(hfs1[i].toString());
+			}
+		
+		assertFalse(evaluate);
+	}
+	
+	
+	@Test
+	public void testGetFields7(){
+		RequestLine rl = new RequestLine("method", "URI", "version");
+		HeaderField hf1 = new HeaderField("Connection","close"); 
+		HeaderField hf2 = new HeaderField("Server","working"); 
+		HeaderField hf3 = new HeaderField("Server","lazy"); 
+		
+		List<HeaderField> fields = new ArrayList<HeaderField>();
+		fields.add(hf1);
+		fields.add(hf2);
+		fields.add(hf3);
+		
+		Header h = new Header(rl, fields);
+		
+		HeaderField[] hfs1 = {hf1,hf2,hf3};
+		HeaderField[] hfs2 = h.getFields();
+		
+		boolean evaluate = true && (hfs1.length == hfs2.length);
+		for(int i=0; i<hfs2.length;i++){
+			evaluate = evaluate && hfs2[i].equals(hfs1[i]);
+			}
+		
+		assertTrue(evaluate);
+		
+	}
+	
+	
+	@Test
+	public void testGetFields8(){
+		RequestLine rl = new RequestLine("method", "URI", "version");
+		HeaderField hf1 = new HeaderField("Connection","close"); 
+		HeaderField hf2 = new HeaderField("Server","working"); 
+		HeaderField hf3 = new HeaderField("Server","lazy"); 
+		HeaderField hf4 = new HeaderField("Server","wrong");
+		
+		List<HeaderField> fields = new ArrayList<HeaderField>();
+		fields.add(hf1);
+		fields.add(hf2);
+		fields.add(hf4);
+		
+		Header h = new Header(rl, fields);
+		
+		HeaderField[] hfs1 = {hf1,hf2,hf3};
+		HeaderField[] hfs2 = h.getFields();
+		
+		boolean evaluate = true && (hfs1.length == hfs2.length);
+		for(int i=0; i<hfs2.length;i++){
+			evaluate = evaluate && hfs2[i].equals(hfs1[i]);
+			}
+		
+		assertFalse(evaluate);
+	}
+	
+	
+	@Test
+	public void testGetFields9(){
+		RequestLine rl = new RequestLine("method", "URI", "version");
+		HeaderField hf1 = new HeaderField("Connection","close"); 
+		HeaderField hf2 = new HeaderField("Server","working"); 
+		HeaderField hf3 = new HeaderField("Server","lazy"); 
+		HeaderField hf4 = new HeaderField("Server","wrong");
+		
+		List<HeaderField> fields = new ArrayList<HeaderField>();
+		fields.add(hf1);
+		fields.add(hf2);
+		fields.add(hf3);
+		fields.add(hf4);
+		
+		Header h = new Header(rl, fields);
+		
+		HeaderField[] hfs1 = {hf1,hf2,hf3,hf4,hf4};
+		HeaderField[] hfs2 = h.getFields();
+		
+		boolean evaluate = true && (hfs1.length == hfs2.length);
+		for(int i=0; i<hfs2.length;i++){
+			evaluate = evaluate && hfs2[i].toString().equals(hfs1[i].toString());
+			}
+		
+		assertFalse(evaluate);
+	}
+	
+	
+	@Test
+	public void testGetFields10(){
+		RequestLine rl = new RequestLine("method", "URI", "version");
+		HeaderField hf1 = new HeaderField("Connection","close"); 
+		HeaderField hf2 = new HeaderField("Server","working"); 
+		HeaderField hf3 = new HeaderField("Server","lazy"); 
+		HeaderField hf4 = new HeaderField("Server","wrong");
+		
+		List<HeaderField> fields = new ArrayList<HeaderField>();
+		fields.add(hf1);
+		fields.add(hf2);
+		fields.add(hf4);
+		
+		Header h = new Header(rl, fields);
+		
+		HeaderField[] hfs1 = {hf1,hf2,hf3,hf4,hf4,hf1};
+		HeaderField[] hfs2 = h.getFields();
+		
+		boolean evaluate = true && (hfs1.length == hfs2.length);
+		for(int i=0; i<hfs2.length;i++){
+			evaluate = evaluate && hfs2[i].equals(hfs1[i]);
+			}
+		
+		assertFalse(evaluate);
+	}
+	
 	// To do --> Andreas
 	@Test
-	public void testSecondConstructor2(){}
+	public void testGetField1(){}
 	
 	
 	// To do --> Andreas
 	@Test
-	public void testSecondConstructor3(){}
+	public void testGetField2(){}
 	
 	
 	// To do --> Andreas
 	@Test
-	public void testSecondConstructor4(){}
+	public void testGetField3(){}
+	
+	
+	// To do --> Andreas
+	@Test
+	public void testGetField4(){}
+	
+	
+	// To do --> Andreas
+	@Test
+	public void testGetField5(){}
+	
+	
+	// To do --> Andreas
+	@Test
+	public void testGetField6(){}
+	
+	
+	// To do --> Andreas
+	@Test
+	public void testGetField7(){}
+	
+
 }
