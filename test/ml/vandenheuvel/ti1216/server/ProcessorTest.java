41d00db3cc7e7f43b6599ab31f41abf368263aab
atheys <andreastheys@gmail.com>
2016-01-16 19:09:06 +0100

    ProcessorTest continued.

---
+++ test/ml/vandenheuvel/ti1216/server/ProcessorTest.java
@@ -0,0 +1,100 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import ml.vandenheuvel.ti1216.*;
+
+public class ProcessorTest {
+
+	/**
+	Weet niet hoe ik dit moet testen. Er is geen vergelijkingsmethode gedefinieerd in zowel 
+	Processor- als DBC-klasse. 
+	
+	@Test
+	public void testConstructor1() {	
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testConstructor2() {
+		fail("Not yet implemented");
+	}
+	*/
+	
+	@Test
+	public void testGetCredentials1() {
+		Credentials cred1 = new Credentials("username1","password1");
+		HeaderField hf1 = new HeaderField("Authorization: "+cred1.);
+		
+	}
+	
+	
+	@Test
+	public void testGetCredentials2() {
+		fail("Not yet implemented");
+	}
+	
+	
+	@Test
+	public void testCheckAuth1() {
+		fail("Not yet implemented");
+	}
+	
+	
+	@Test
+	public void testCheckAuth2() {
+		fail("Not yet implemented");
+	}
+	
+	
+	@Test
+	public void testProcessUser1() {
+		fail("Not yet implemented");
+	}
+	
+	
+	@Test
+	public void testProcessUser2() {
+		fail("Not yet implemented");
+	}
+	
+	
+	@Test
+	public void testProcessChat1() {
+		fail("Not yet implemented");
+	}
+	
+	
+	@Test
+	public void testProcessChat2() {
+		fail("Not yet implemented");
+	}
+	
+	
+	@Test
+	public void testProcessFaculty1() {
+		fail("Not yet implemented");
+	}
+	
+	
+	@Test
+	public void testProcessFaculty2() {
+		fail("Not yet implemented");
+	}
+	
+
+	@Test
+	public void testProcess1() {
+		fail("Not yet implemented");
+	}
+	
+	
+	@Test
+	public void testProcess2() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testProcess3() {
+		fail("Not yet implemented");
+	}
+	
+
+}
