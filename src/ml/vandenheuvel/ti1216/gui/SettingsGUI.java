5b46b86a558692007330f3927d4c0d0d81755b24
Al5tfrq3BRrIbV <heuveltje@hotmail.com>
2016-01-18 18:15:06 +0100

    Nodig

--- src/ml/vandenheuvel/ti1216/gui/SettingsGUI.java
+++ src/ml/vandenheuvel/ti1216/gui/SettingsGUI.java
@@ -1,3 +1,15 @@
+package ml.vandenheuvel.ti1216.gui;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import ml.vandenheuvel.ti1216.client.ServerCommunicator;
 
 /**
@@ -18,8 +30,13 @@ public class SettingsGUI {
 		 * database.
 		 */
 		Stage window = new Stage();
+		window.initModality(Modality.APPLICATION_MODAL);
 		window.setTitle("SettingsGUI");
+		ServerCommunicator.login(MainGUI.credentials);
 
+		/**
+		 * Sets the constraints of the window.
+		 */
 		VBox vbox = new VBox();
 		HBox hbox1 = new HBox();
 		HBox hbox2 = new HBox();
@@ -90,6 +107,9 @@ public class SettingsGUI {
 		BorderPane borderPane = new BorderPane();
 		borderPane.setCenter(vbox);
 
+		/**
+		 * Sets the seize of the window and adds all the new elements.
+		 */
 		Scene scene = new Scene(borderPane, 350, 200);
 		window.setScene(scene);
 		window.showAndWait();
