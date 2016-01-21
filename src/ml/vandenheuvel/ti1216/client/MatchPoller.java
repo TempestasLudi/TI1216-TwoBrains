f9e054c2ea0dab4d5ef34ac5cc0296f995ccdf11
Al5tfrq3BRrIbV <heuveltje@hotmail.com>
2016-01-21 01:45:01 +0100

    Logging voor MatchPoller.

--- src/ml/vandenheuvel/ti1216/client/MatchPoller.java
+++ src/ml/vandenheuvel/ti1216/client/MatchPoller.java
@@ -1,6 +1,8 @@
 package ml.vandenheuvel.ti1216.client;
 
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import ml.vandenheuvel.ti1216.data.Credentials;
 import ml.vandenheuvel.ti1216.data.Match;
@@ -9,6 +11,8 @@ import ml.vandenheuvel.ti1216.data.Match;
  * MatchPoller polls to the server for new matches.
  */
 public class MatchPoller implements Runnable{
+	
+	private static Logger logger = Logger.getLogger("ml.vandenheuvel.ti1216.client");
 	
 	/**
 	 * Whether the poller should keep polling or not.
@@ -26,6 +30,11 @@ public class MatchPoller implements Runnable{
 	private ClientManager manager;
 	
 	/**
+	 * The amount of milliseconds to wait before polling
+	 */
+	private int interval = 1000;
+	
+	/**
 	 * Class constructor.
 	 * 
 	 * @param credentials the credentials of the user matches should be fetched for
@@ -41,26 +50,31 @@ public class MatchPoller implements Runnable{
 	 */
 	@Override
 	public void run() {
+		logger.info("Started 'run()' method in MatchPoller.");
 		while (this.run) {
+			logger.fine("Fetching matches...");
 			ArrayList<Match> matches = ServerCommunicator.fetchMatches(this.credentials);
+			logger.fine("Received " + Integer.toString(matches.size()) + " matches.");
 			for (int i = 0; i < matches.size(); i++) {
 				this.manager.incomingMatch(matches.get(i));
 			}
 			if (matches.isEmpty()) {
 				try {
-					Thread.sleep(1000);
+					logger.finest("Sleeping for " + Integer.toString(this.interval) + " milliseconds.");
+					Thread.sleep(this.interval);
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					logger.log(Level.WARNING, "Failed sleeping for " + Integer.toString(this.interval), e);
 				}
 			}
 		}
+		logger.info("MatchPoller thread stopped.");
 	}
 	
 	/**
 	 * Lets the poller terminate after the next poll.
 	 */
 	public void stop() {
+		logger.fine("Stopping MatchPoller thread...");
 		this.run = false;
 	}
 	
